95fc4982e6e01b516c93e19b31d86e681aa4617b
Skelly Kiernan <skelly@example.com>
2016-02-01 15:55:00 +0000

    files to edit as bash variable

--- .tmux/time-mgmt.sh
+++ .tmux/time-mgmt.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 working_dir="/home/skelly/Dropbox/coursera/time_mgmt"
 session_name="time-mgmt"
+edit_files="notes.md build.sh"
 
 tmux new-session -d -s ${session_name}  
-tmux new-window -c ${working_dir} -t ${session_name} -n 'edit' 'vim build.sh notes.md'
+tmux new-window -c ${working_dir} -t ${session_name} -n "edit" "vim ${edit_files}"
 # ensure only default window is removed, leaving a sinlge window
 tmux kill-window -t 0 # target is the window index
 tmux split-window -c ${working_dir} -t ${session_name} -v -p 30
